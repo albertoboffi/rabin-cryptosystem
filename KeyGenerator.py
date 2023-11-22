3d608d2bc58588160a066e221b77dbd2b91fc1be
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-11-22 10:47:03 +0100

    feat: implement key generator

--- KeyGenerator.py
+++ KeyGenerator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import secrets
 from primality import primality
 
 __author__ = 'Alberto Boffi'
@@ -7,4 +8,49 @@ __date__ = '2023-11-23'
 __deprecated__ = False
 
 class KeyGenerator:
-    pass
+    
+    # Input: prime p
+    # Output True if p ≡ 3 mod 4, False otherwise
+    def __isValidPrime(self, p):
+
+        if (p % 4 == 3): return True
+        return False
+    
+    # Input: -
+    # Output: random number n, st the generated prime will be at least the n-th prime
+    def __getRandomSeed(self):
+
+        seed_range = range(1000, 10000)
+        seed = secrets.choice(seed_range)
+
+        return seed
+    
+    # Input: -
+    # Output: random large prime p such that p ≡ 3 mod 4
+    def __generatePrime(self):
+
+        seed = self.__getRandomSeed()
+
+        p = primality.nthprime(seed)
+
+        i = 0
+        while (not(self.__isValidPrime(p))):
+           p = primality.nthprime(seed + i)
+           i += 1
+
+        return p
+
+    # Input: -
+    # Output: key pair for the Rabin cryptosystem
+    def generateKeys(self):
+
+        p = self.__generatePrime()
+        q = self.__generatePrime()
+
+        k_pri = p * q
+        k_pub = (p, q)
+
+        return {
+            "private": k_pri,
+            "public": k_pub
+        }
