deb0fffa380fb6a4bb2f193f1a4015d158669604
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-11-22 09:19:36 +0100

    feat: create class backbone

---
+++ RabinCryptosystem.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+__author__ = 'Alberto Boffi'
+__date__ = '2023-11-23'
+__deprecated__ = False
+
+class RabinCryptosystem:
+
+    def __generateKeys():
+        pass
+
+    def __init__(self):
+        keys = self.__generateKeys()
+
+        self.k_pri = keys["private"]
+        self.k_pub = keys["public"]
+
+    def encrypt(self, msg, key):
+        pass
+
+    def decrypt(self, msg, key):
+        pass
