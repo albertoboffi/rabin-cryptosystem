511c2a67104aeb90fa55f9343e180c6bad6d85f9
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-11-22 11:16:40 +0100

    feat: add key generation feature

--- src/RabinCryptosystem.py
+++ src/RabinCryptosystem.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
+from KeyGenerator import KeyGenerator
+
 __author__ = 'Alberto Boffi'
 __date__ = '2023-11-23'
 __deprecated__ = False
 
 class RabinCryptosystem:
 
-    def __generateKeys():
-        pass
-
+    # Input: -
+    # Output: -
+    # Behavior: Initialize the key generator and generates the keys
     def __init__(self):
-        keys = self.__generateKeys()
 
+        self.key_generator = KeyGenerator()
+        self.generateKeys()
+
+    # Input: -
+    # Output: -
+    # Behavior: Generates a key pair
+    def generateKeys(self):
+
+        keys = self.key_generator.generateKeys()
         self.k_pri = keys["private"]
         self.k_pub = keys["public"]
 
