ce1d74267662540df8546f1e26fe5e2616dc5bb7
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-12-03 10:14:58 +0100

    feat: implement decryption

--- src/RabinCryptosystem.py
+++ src/RabinCryptosystem.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-from KeyGenerator import KeyGenerator
+from .KeyGenerator import KeyGenerator
+from .algorithms import *
+
+from decimal import *
+getcontext().prec = 1000000 # adjust precision to deal with large numbers
 
 __author__ = 'Alberto Boffi'
 __deprecated__ = False
@@ -10,6 +14,7 @@ class RabinCryptosystem:
     # Input: -
     # Output: -
     # Behavior: Initialize the key generator and generates the keys
+    
     def __init__(self):
 
         self.key_generator = KeyGenerator()
@@ -18,6 +23,7 @@ class RabinCryptosystem:
     # Input: -
     # Output: -
     # Behavior: Generates a key pair
+
     def generateKeys(self):
 
         keys = self.key_generator.generateKeys()
@@ -36,13 +42,51 @@ class RabinCryptosystem:
     # Input: plaintext
     # Output: ciphertext
     # Behavior: Encrypts the plaintext using the public key
-    def encrypt(self, plaintext: int):
+
+    def encrypt(self, plaintext: int) -> int:
 
         ciphertext = (plaintext ** 2) % self.k_pub
 
         return ciphertext
 
+    # Input: ciphertext
+    # Output: All four possible plaintexts
+    # Behavior: Decrypts the ciphertext using the private and the public key
 
-    def decrypt(self, ciphertext: int):
+    def decrypt(self, ciphertext: int) -> int:
 
-        pass
+        # prime numbers composing the private key
+
+        p = self.k_pri[0]
+        q = self.k_pri[1]
+
+        # square roots modulo private key
+
+        sq_p = ciphertext ** (Decimal('0.25') * (p + 1)) % p
+        sq_q = ciphertext ** (Decimal('0.25') * (q + 1)) % q
+
+        sq_p = int(sq_p)
+        sq_q = int(sq_q)
+
+        # bezout coefficients
+
+        coef = extEuclideanAlgorithm(p, q)["coef"]
+
+        coef_p = coef[0]
+        coef_q = coef[1]
+
+        # square roots
+
+        plaintext_1 = (coef_p * p * sq_q + coef_q * q * sq_p) % self.k_pub
+        plaintext_2 = self.k_pub - plaintext_1
+        plaintext_3 = (coef_p * p * sq_q - coef_q * q * sq_p) % self.k_pub
+        plaintext_4 = self.k_pub - plaintext_3
+
+        return [
+
+            plaintext_1,
+            plaintext_2,
+            plaintext_3,
+            plaintext_4
+
+        ]
