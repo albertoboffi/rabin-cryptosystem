5458de7c579f0cafc30c28bd6ae288df344d4337
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-12-03 09:25:51 +0100

    feat: implement system solver using crm

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from math import prod
+
 __author__ = 'Alberto Boffi'
 __deprecated__ = False
 
@@ -26,4 +28,22 @@ def extEuclideanAlgorithm(r0: int, r1: int, s0 = 1, s1 = 0, t0 = 0, t1 = 1):
 
     }
 
+# Input: List a of residues, list m of moduluses. It must holds that len(a) = len(m)
+# Output: Result of the system
+# Behavior: Solves the system using the Chinese Remainder Theorem
+
+def chineseRemainderTheorem(a: list, m: list):
+
+    x = 0
+
+    for i in range(len(a)):
+
+        M = prod(m) / m[i] # products of all the moduluses except m[i]
+
+        coef = extEuclideanAlgorithm(M, m[i])["coef"]
+        coef_M = coef[0] # coefficient of M
+
+        x += M * coef_M * a[i] # add the solution of the single congruence to the overall solution
+
+    return x
 
