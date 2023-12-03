4703991546f23ba42284de2bfad9f84f71d97d55
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-12-03 10:47:12 +0100

    docs: add sample script

---
+++ examples/encryption.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+#---------------------------------------------------#
+#--------------- Prepare for import ----------------#
+#---------------- IGNORE THIS PART -----------------#
+#---------------------------------------------------#
+
+import os, sys
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+            )
+        )
+    )
+
+from src.RabinCryptosystem import RabinCryptosystem
+
+#---------------------------------------------------#
+#--------------- Possible messages -----------------#
+#----------- CHANGE WITH YOUR OWN MESAGE -----------#
+#---------------------------------------------------#
+
+msg = 741
+
+#---------------------------------------------------#
+#--------------- Body of the script ----------------#
+#------------------ DO NOT TOUCH -------------------#
+#---------------------------------------------------#
+
+def main(msg):
+
+    # encryption
+
+    cryptosystem = RabinCryptosystem()
+
+    ciphertext = cryptosystem.encrypt(msg)
+    plaintexts = cryptosystem.decrypt(ciphertext)
+
+    # log
+
+    print("Original message >> ", msg)
+    print("Encrypted message >> ", ciphertext)
+    print("Possible plaintexts >> ", plaintexts)
+
+
+if __name__ == '__main__':
+
+    main(msg)
